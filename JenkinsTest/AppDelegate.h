d69e0c3523b476792ffc9abc5c3d2a0b9f973d16
djsunqi <sunqi@imdaad.cn>
2019-09-24 13:48:35 +0800

    Initial Commit

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by sunqi on 2019/9/24.
+//  Copyright © 2019 _____DJ_____. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by sunqi on 2019/9/24.
+//  Copyright © 2019 _____DJ_____. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
